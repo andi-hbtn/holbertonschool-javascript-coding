419c008d6c6267092d981106304459f3060f3833
andi-hbtn <andi.bevapi@holbertonschool.com>
2024-01-22 14:04:58 +0100

    changes

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,13 +3,6 @@ const request = require("request");
 const apiUrl = process.argv[2];
 
 request.get(apiUrl, (err, response, body) => {
-    // if (response.statusCode === 200) {
-    //     console.log({ code: response.statusCode });
-    // }
-    if (err) {
-        console.error(err);
-        return err
-    }
     console.log({ code: response.statusCode });
 })
 
