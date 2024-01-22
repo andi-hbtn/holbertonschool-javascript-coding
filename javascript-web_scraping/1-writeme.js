a2568181858aa7aa6ff9b2033f23677cf97c331e
andi-hbtn <andi.bevapi@holbertonschool.com>
2024-01-22 13:08:38 +0100

    changes

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fileName = process.argv[2];
 const textToInsert = process.argv[3];
 
 
-fs.writeFile('my_file.txt', textToInsert, { encoding: 'utf8', flag: 'a+' }, err => {
+fs.writeFile(fileName, textToInsert, { encoding: 'utf8', flag: 'a+' }, err => {
     if (err) {
         console.error(err);
     } else {
